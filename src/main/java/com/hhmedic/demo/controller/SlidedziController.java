a3b629d4418c9be2dfc96124f8d2913aa69bea82
lcssos <lcs@hh-medic.com>
2017-07-18 10:25:53 +0800

    更新瓦片

--- src/main/java/com/hhmedic/demo/controller/SlidedziController.java
+++ src/main/java/com/hhmedic/demo/controller/SlidedziController.java
@@ -14,24 +14,6 @@ public class SlidedziController {
 	@ResponseBody
 	public String detail(@PathVariable String id){
 
-		//60483, 110059		16
-		//30241, 55029		15
-		//15120, 27514		14
-		//7560, 13757		13
-		//3780, 6878		12
-		//1890, 3439		11
-		//945, 1719			10
-		//472, 859			9
-		//236, 429			8
-		//118, 214			7
-		//59, 107			6
-		//29, 53			5
-		//14, 26			4
-		//7, 13				3
-		//3, 6				2
-		//1, 2				1
-		//1, 1				0
-
 		if("v2".equals(id)){
 			return "<?xml version='1.0' encoding='UTF-8'?>\n<Image Url=\"http://imgs.hh-medic.com/pathology/0/K1703243-%E5%AE%89%E7%8E%89%E9%A6%99_kfb_0/\" Format=\"jpg\" Overlap=\"0\" TileSize=\"256\" xmlns=\"http://schemas.hh-medic.com/deepzoom/2008\"><Size Height=\"60483\" Width=\"110059\" /></Image>";
 		}
