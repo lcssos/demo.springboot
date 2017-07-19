509e2185276747bf4961369cff60d18de0a1b198
lcssos <lcs@hh-medic.com>
2017-07-19 18:23:21 +0800

    更新瓦片

--- src/main/java/com/hhmedic/demo/controller/SlidedziController.java
+++ src/main/java/com/hhmedic/demo/controller/SlidedziController.java
@@ -18,7 +18,7 @@ public class SlidedziController {
 			return "<?xml version='1.0' encoding='UTF-8'?>\n<Image Url=\"http://imgs.hh-medic.com/pathology/0/K1703243-%E5%AE%89%E7%8E%89%E9%A6%99_kfb_0/\" Format=\"jpg\" Overlap=\"0\" TileSize=\"256\" xmlns=\"http://schemas.hh-medic.com/deepzoom/2008\"><Size Height=\"60483\" Width=\"110059\" /></Image>";
 		}
 
-		return "<?xml version='1.0' encoding='UTF-8'?>\n<Image Url=\"/slide_files/\" Format=\"jpeg\" Overlap=\"1\" TileSize=\"254\" xmlns=\"http://schemas.microsoft.com/deepzoom/2008\"><Size Height=\"210579\" Width=\"94968\" /></Image>";
+		return "<?xml version='1.0' encoding='UTF-8'?>\n<Image Url=\"http://imgs.hh-medic.com/pathology/patient_001_node_1/slide_files/\" Format=\"jpeg\" Overlap=\"1\" TileSize=\"254\" xmlns=\"http://schemas.microsoft.com/deepzoom/2008\"><Size Height=\"210579\" Width=\"94968\" /></Image>";
 	}
 
 }
